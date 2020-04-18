b520062332a4d301406898c150ad4530a236c918
4thel00z <4thel00z@gmail.com>
2020-04-18 14:36:54 +0200

    [master] Change the repo for import path. Test relocation

--- main.go
+++ main.go
@@ -5,10 +5,30 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type URLRedirect struct {
+	Original  string `json:"original"`
+	Shortened string `json:"shortened"`
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	/*
+			authToken, ok := os.LookupEnv("FAUNADB_SERVER_SECRET")
+
+			if !ok {
+				return events.APIGatewayProxyResponse{
+					StatusCode: 500,
+					Body:       "Could not connect to Fauna!",
+				}, nil
+			}
+		client := f.NewFaunaClient(authToken)
+	*/
+
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Headers: map[string]string{
+			"Location": "https://pythonmagic.com",
+		},
+		StatusCode: 301,
+		Body: nil, //      "Hello AWS Lambda and Netlify",
 	}, nil
 }
 
